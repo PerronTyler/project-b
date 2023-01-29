3ecbb09ff92a3bb099760160d36f10392c30ad21
Judah Mendoza <judahramses@gmail.com>
2023-01-29 11:39:38 -0800

    changes to navbar and landing page content w/ sampel imgs

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,18 +3,21 @@ import { Link, Outlet } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <header className='bg-slate-900'>
-            <nav>
-                <div>
-                    <Link className='text-white' to="/#">Services</Link>
-                    <Link className='text-white' to="/#">About Us</Link>
-                    <Link className='text-blue-500' to="/">Apex Construction</Link>
-                    <Link className='text-white' to="/#">Gallery</Link>
-                    <Link className='text-white' to="/#">Contact</Link>
-                    <Outlet />
-                </div>
-            </nav>
-        </header>
+        <div>
+            <header className='bg-slate-900'>
+                <nav>
+                    <div className='display: flex flex-1 justify-evenly p-4'>
+                        <Link className='text-white' to="/#">Services</Link>
+                        <Link className='text-white' to="/#">About Us</Link>
+                        <Link className='text-blue-500 font-bold' to="/">Apex Craftsmen Inc</Link>
+                        <Link className='text-white' to="/#">Gallery</Link>
+                        <Link className='text-white' to="/#">Contact</Link>
+                    </div>
+                </nav>
+                <hr />
+                <Outlet />
+            </header>
+        </div>
     )
 }
 
--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Main = () => {
     return (
         <div>
-            <div className='grid grid-cols-2 gap-4'>
+            <div className='grid grid-cols-2 gap-4 bg-slate-900 p-4'>
                 <div>
                     <h1 className='font-medium leading-tight text-5xl mt-0 mb-2 text-white'>
                         We help to <br />
@@ -12,16 +12,50 @@ const Main = () => {
                     <p className='text-white'>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's sstandard dummy text ever since the 1500s, when an unknown printer took a gallery of type and scrambled it to make Aldus PageMaker including version of Lorem Ipsum.</p>
                 </div>
                 <div>
-                    <img src="" alt="pic" />
+                    <img src="https://images.unsplash.com/photo-1598368195835-91e67f80c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="pic" />
                 </div>
             </div>
             <div className='bg-green-500'>
-                <h1>Over 25 years of experience</h1>
-                <a href="/#">Get A Quote</a>
-                <div className='grid grid-cols-3 gap-4'>
-                    <div>1st Service</div>
-                    <div>2nd Service</div>
-                    <div>3rd Service</div>
+                <img src="https://images.unsplash.com/photo-1482731215275-a1f151646268?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="pic" />
+                <h1 className='text-center'>Over 25 years of experience</h1>
+                <p className='text-center'>
+                    <a href="/#">Request info</a>
+                </p>
+                <div className='grid grid-cols-3 gap-4 text-center'>
+                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <img className="rounded-t-lg" src="https://images.unsplash.com/photo-1601760561441-16420502c7e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="pic" />
+                        <div className="p-5">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kitchens</h5>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">placeholder text.</p>
+                            <a href="/#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Browse Now
+                                <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <img className="rounded-t-lg" src="https://images.unsplash.com/photo-1521783593447-5702b9bfd267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1204&q=80" alt="pic" />
+                        <div className="p-5">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Baths</h5>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">placeholder text.</p>
+                            <a href="/#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Browse Now
+                                <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <img className="rounded-t-lg" src="https://images.unsplash.com/photo-1601428243305-25c850953aae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="pic" />
+                        <div className="p-5">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Decks</h5>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">placeholder text.</p>
+                            <a href="/#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Browse Now
+                                <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </a>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
             <div className='bg-white'>
@@ -35,6 +69,10 @@ const Main = () => {
                         <h2>Daily Consultant</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
+                    <div>
+                        <hr />
+                    </div>
+                    <div></div>
                     <div>
                         <h2>Custom Design</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
