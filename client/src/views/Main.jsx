590f7294c39c67160b118a9d622a0267b264a16e
Judah Mendoza <judahramses@gmail.com>
2023-01-29 22:16:45 -0800

    worked on adding images to home page and building out card components with images of less memeory. Included assets folder for now until backend is complete.

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -5,81 +5,92 @@ const Main = () => {
         <div>
             <div className='grid grid-cols-2 gap-4 bg-slate-900 p-4'>
                 <div>
-                    <h1 className='font-medium leading-tight text-5xl mt-0 mb-2 text-white'>
+                    <h1 className='font-medium leading-tight text-5xl mt-0 mb-2 pt-11 text-white text-center'>
                         We help to <br />
-                        build your <br />
-                        dream.</h1>
-                    <p className='text-white'>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's sstandard dummy text ever since the 1500s, when an unknown printer took a gallery of type and scrambled it to make Aldus PageMaker including version of Lorem Ipsum.</p>
+                        build your dream.</h1>
+                    <p className='text-white text-center'>Building with vision, quality, and pride.
+                    </p>
                 </div>
-                <div>
-                    <img src="https://images.unsplash.com/photo-1598368195835-91e67f80c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="pic" />
-                </div>
+                <div className='right-image-1'></div>
             </div>
-            <div className='bg-green-500'>
-                <img src="https://images.unsplash.com/photo-1482731215275-a1f151646268?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="pic" />
-                <h1 className='text-center'>Over 25 years of experience</h1>
-                <p className='text-center'>
-                    <a href="/#">Request info</a>
-                </p>
-                <div className='grid grid-cols-3 gap-4 text-center'>
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <img className="rounded-t-lg" src="https://images.unsplash.com/photo-1601760561441-16420502c7e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="pic" />
-                        <div className="p-5">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kitchens</h5>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">placeholder text.</p>
-                            <a href="/#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Browse Now
-                                <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </a>
+            <div className='bg-image'>
+                <div>
+                    <h1 className='text-4xl font-bold text-center p-4'>Over 25 years of experience</h1>
+                    <p className='text-center'><a className='call-us-button' href="/#">Call us now</a></p>
+                </div>
+                <div className='display: flex justify-around flex-1 flex-wrap'>
+                    <div className="max-w-sm bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 card-image-1">
+                        <div className=''>
+                            <h5 className="mb-2 text-4xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Kitchens</h5>
+                            <p className='text-center'><a className='browse-now-button' href="/#">Browse Now<svg
+                                className="ml-2 -mr-1 h-4 w-4"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                />
+                            </svg></a></p>
                         </div>
                     </div>
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <img className="rounded-t-lg" src="https://images.unsplash.com/photo-1521783593447-5702b9bfd267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1204&q=80" alt="pic" />
-                        <div className="p-5">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Baths</h5>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">placeholder text.</p>
-                            <a href="/#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Browse Now
-                                <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </a>
+                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 card-image-2">
+                        <div>
+                            <h5 className="mb-2 text-4xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Baths</h5>
+                            <p className='text-center'><a className='browse-now-button' href="/#">Browse Now<svg
+                                className="ml-2 -mr-1 h-4 w-4"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                />
+                            </svg></a></p>
                         </div>
                     </div>
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <img className="rounded-t-lg" src="https://images.unsplash.com/photo-1601428243305-25c850953aae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="pic" />
-                        <div className="p-5">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Decks</h5>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">placeholder text.</p>
-                            <a href="/#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Browse Now
-                                <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </a>
+                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 card-image-3">
+                        <div>
+                            <h5 className="mb-2 text-4xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Decks</h5>
+                            <p className='text-center'><a className='browse-now-button' href="/#">Browse Now<svg
+                                className="ml-2 -mr-1 h-4 w-4"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                />
+                            </svg></a></p>
                         </div>
                     </div>
-                    
                 </div>
             </div>
             <div className='bg-white'>
-                <h1>WHY CHOOSE US</h1>
-                <div className='grid grid-cols-2 gap-2'>
+                <label className='text-4xl p-4'><span className='text-6xl align-middle'>&#42;</span> WHY CHOOSE US</label>
+                <div className='grid grid-cols-2 gap-2 p-4'>
                     <div>
-                        <h2>The Best Price</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <label>01</label> <span>The Best Price</span>
+                        <p>A project is an activity to meet the creation of a unique product of service and thus activities that are under taken to accomodate outline.</p>
                     </div>
                     <div>
-                        <h2>Daily Consultant</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <label>02</label> <span>Daily Consultant</span>
+
+                        <p>One of the most common competency based on questions for any sequiring some project that make client for easy consulting about the property.</p>
                     </div>
                     <div>
-                        <hr />
-                    </div>
-                    <div></div>
-                    <div>
-                        <h2>Custom Design</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <label>03</label> <span>Custom Design</span>
+                        <p>A project is defined as a sequence of tasks that must be completed to attain a certain outcome. According to the project with custom designs</p>
                     </div>
                     <div>
-                        <h2>Premium Quality</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <label>04</label> <span>Premium Quality</span>
+                        <p>Make sure that everything we use is premium quality and taking no longer than deadlines set. This will get the ball rolling and improve your confidence.</p>
                     </div>
                 </div>
                 <div className='grid grid-rows-2 grid-flow-col gap-4 bg-slate-900'>
