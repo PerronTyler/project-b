202bdb6c8c6da9a4da436a7074b359912ba086a2
Judah Mendoza <judahramses@gmail.com>
2023-02-22 20:53:25 -0800

    tweaked the image gallery array to one big object with categories for filter functionality on the gallery page.

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,48 +1,43 @@
 import React from 'react'
+import { useState } from 'react'
 // import BathImgs from '../images/GalleryAssets/BathImgs'
 // import DeckImgs from '../images/GalleryAssets/DeckImgs'
 // import KitchenImgs from '../images/GalleryAssets/KitchenImgs'
 // import RemodelImgs from '../images/GalleryAssets/RemodelImgs'
-import { BATHS, DECKS, KITCHENS, REMODELS } from './Images.js'
+import { GALLERYDATA } from './Images.js'
 
 const Gallery = () => {
+    const [products, setProducts] = useState(GALLERYDATA)
+
+    const filterCategory = (categoryItem) => {
+        const result = GALLERYDATA.filter((currentCategory) => {
+            return currentCategory.category === categoryItem;
+        });
+        setProducts(result);
+    }
 
     return (
         <>
             <h1 className='text-center text-4xl mt-10'>Our Showcase</h1>
             <hr className='gallery-line' />
-            <p className='ml-24 mr-24 text-zinc-400'>When Apex Construction takes on a project, it's personal. It's about listening and understanding your desires and then delivering the best service, quality, and value throughout your experience. All our remodeling services are one of a kind because they are built with you in mind. We welcome you to discover a selection of remodeled projects our clients have entrusted us to build here in South Shore Massachusetts.</p>
+            <p className='mt-10 mb-10 ml-24 mr-24 text-zinc-400'>When Apex Construction takes on a project, it's personal. It's about listening and understanding your desires and then delivering the best service, quality, and value throughout your experience. All our remodeling services are one of a kind because they are built with you in mind. We welcome you to discover a selection of remodeled projects our clients have entrusted us to build here in South Shore Massachusetts.</p>
+            <div className='text-center mt-10 mb-10'>
+                <h4>Filter by Category</h4>
+                <div className='p-4'>
+                    <button className='btns' onClick={() => setProducts(GALLERYDATA)}>All</button>
+                    <button className='btns' onClick={() => filterCategory('Baths')}>Baths</button>
+                    <button className='btns' onClick={() => filterCategory('Kitchens')}>Kitchens</button>
+                    <button className='btns' onClick={() => filterCategory('Decks')}>Decks</button>
+                    <button className='btns' onClick={() => filterCategory('Remodels')}>Remodels</button>
+                </div>
+            </div>
             <div className='gallery-container'>
-                <div className='grid grid-cols-4 gap-4 items-center justify-items-center mt-4 mb-4'>
+                <div className='grid grid-cols-4 gap-4 items-center justify-items-center p-4 mt-4 mb-4'>
                     {
-                        BATHS && BATHS.map((item) =>
-                        <div key={item.id}>
-                            <img className='gallery-pic gallery-effect' src={item.image} alt="pic" />
-                            <p className='text-center text-stone-500 mt-2'>{item.name}</p>
-                        </div>
-                        )
-                    }
-                    {
-                        KITCHENS && KITCHENS.map((item) =>
-                        <div key={item.id}>
-                            <img className='gallery-pic gallery-effect' src={item.image} alt="pic" />
-                            <p className='text-center text-stone-500 mt-2'>{item.name}</p>
-                        </div>
-                        )
-                    }
-                    {
-                        DECKS && DECKS.map((item) =>
-                        <div key={item.id}>
-                            <img className='gallery-pic gallery-effect' src={item.image} alt="pic" />
-                            <p className='text-center text-stone-500 mt-2'>{item.name}</p>
-                        </div>
-                        )
-                    }
-                    {
-                        REMODELS && REMODELS.map((item) =>
-                        <div key={item.id}>
-                            <img className='gallery-pic gallery-effect' src={item.image} alt="pic" />
-                            <p className='text-center text-stone-500 mt-2'>{item.name}</p>
+                        products.map((product) =>
+                        <div key={product.id}>
+                            <img className='gallery-pic gallery-effect' src={product.image} alt="pic" />
+                            <p className='text-center text-stone-500 mt-2'>{product.name}</p>
                         </div>
                         )
                     }
--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -1,170 +1,193 @@
-export const BATHS = [
+export const GALLERYDATA = [
     {
         id: 1,
+        category: 'Baths',
         name: 'Lavender Loo',
         image: 'images/BathImgs/Bath01.jpg'
     },
     {
         id: 2,
+        category: 'Baths',
         name: 'Rustic Retreat',
         image: 'images/BathImgs/Bath02.jpg'
     },
     {
         id: 3,
+        category: 'Baths',
         name: 'Powder Room Paradise',
         image: 'images/BathImgs/Bath03.jpg'
     },
     {
         id: 4,
+        category: 'Baths',
         name: 'Tropical Throne',
         image: 'images/BathImgs/Bath04.jpg'
     },
     {
         id: 5,
+        category: 'Baths',
         name: 'Spa Sanctuary',
         image: 'images/BathImgs/Bath05.jpg'
     },
     {
         id: 6,
+        category: 'Baths',
         name: 'Zen Zone',
         image: 'images/BathImgs/Bath06.jpg'
     },
     {
         id: 7,
+        category: 'Baths',
         name: 'Aqua Oasis',
         image: 'images/BathImgs/Bath07.jpg'
     },
     {
         id: 8,
+        category: 'Baths',
         name: 'Boho Bliss',
         image: 'images/BathImgs/Bath08.jpg'
-    }
-]
-
-export const DECKS = [
+    },
     {
-        id: 1,
+        id: 9,
+        category: 'Decks',
         name: 'Coastal Cabana',
         image: 'images/DeckImgs/Deck01.jpg'
     },
     {
-        id: 2,
+        id: 10,
+        category: 'Decks',
         name: 'Rustic Veranda',
         image: 'images/DeckImgs/Deck02.jpg'
     },
     {
-        id: 3,
+        id: 11,
+        category: 'Decks',
         name: 'Garden Oasis',
         image: 'images/DeckImgs/Deck03.jpg'
     },
     {
-        id: 4,
+        id: 12,
+        category: 'Decks',
         name: 'Mountain View',
         image: 'images/DeckImgs/Deck04.jpg'
     },
     {
-        id: 5,
+        id: 13,
+        category: 'Decks',
         name: 'Zen Retreat',
         image: 'images/DeckImgs/Deck05.jpg'
     },
     {
-        id: 6,
+        id: 14,
+        category: 'Decks',
         name: 'Urban Skyline',
         image: 'images/DeckImgs/Deck06.jpg'
     },
     {
-        id: 7,
+        id: 15,
+        category: 'Decks',
         name: 'Medi Patio',
         image: 'images/DeckImgs/Deck07.jpg'
     },
     {
-        id: 8,
+        id: 16,
+        category: 'Decks',
         name: 'Lakeside Haven',
         image: 'images/DeckImgs/Deck08.jpg'
-    }
-]
-
-export const KITCHENS = [
+    },
     {
-        id: 1,
+        id: 17,
+        category: 'Kitchens',
         name: 'Cuisine Studio',
         image: 'images/KitchenImgs/Kitchen01.jpg'
     },
     {
-        id: 2,
+        id: 18,
+        category: 'Kitchens',
         name: 'Coastal Cookery',
         image: 'images/KitchenImgs/Kitchen02.jpg'
     },
     {
-        id: 3,
+        id: 19,
+        category: 'Kitchens',
         name: 'Tuscan Kitchenette',
         image: 'images/KitchenImgs/Kitchen03.jpg'
     },
     {
-        id: 4,
+        id: 20,
+        category: 'Kitchens',
         name: 'Chic Galley',
         image: 'images/KitchenImgs/Kitchen04.jpg'
     },
     {
-        id: 5,
+        id: 21,
+        category: 'Kitchens',
         name: 'Zen Cooking',
         image: 'images/KitchenImgs/Kitchen05.jpg'
     },
     {
-        id: 6,
+        id: 22,
+        category: 'Kitchens',
         name: 'Kitchen Loft',
         image: 'images/KitchenImgs/Kitchen06.jpg'
     },
     {
-        id: 7,
+        id: 23,
+        category: 'Kitchens',
         name: 'Gourmet Galley',
         image: 'images/KitchenImgs/Kitchen07.jpg'
     },
     {
-        id: 8,
+        id: 24,
+        category: 'Kitchens',
         name: 'Art Deco',
         image: 'images/KitchenImgs/Kitchen08.jpg'
-    }
-]
-
-export const REMODELS = [
+    },
     {
-        id: 1,
+        id: 25,
+        category: 'Remodels',
         name: 'Serenity',
         image: 'images/RemodelImgs/Remodel01.jpg'
     },
     {
-        id: 2,
+        id: 26,
+        category: 'Remodels',
         name: 'Harmony',
         image: 'images/RemodelImgs/Remodel02.jpg'
     },
     {
-        id: 3,
+        id: 27,
+        category: 'Remodels',
         name: 'Tranquil Abode',
         image: 'images/RemodelImgs/Remodel03.jpg'
     },
     {
-        id: 4,
+        id: 28,
+        category: 'Remodels',
         name: 'Coastal Cottage',
         image: 'images/RemodelImgs/Remodel04.jpg'
     },
     {
-        id: 5,
+        id: 29,
+        category: 'Remodels',
         name: 'Mordern Oasis',
         image: 'images/RemodelImgs/Remodel05.jpg'
     },
     {
-        id: 6,
+        id: 30,
+        category: 'Remodels',
         name: 'Sunny Villa',
         image: 'images/RemodelImgs/Remodel06.jpg'
     },
     {
-        id: 7,
+        id: 31,
+        category: 'Remodels',
         name: 'Charming Bungalow',
         image: 'images/RemodelImgs/Remodel07.jpg'
     },
     {
-        id: 8,
+        id: 32,
+        category: 'Remodels',
         name: 'Urban Chic',
         image: 'images/RemodelImgs/Remodel08.jpg'
     }
